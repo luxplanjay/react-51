56be099a69966e92ddc284677d1164e6395e8d92
Alexander Repeta <alexander.repeta@gmail.com>
2022-10-04 13:44:04 +0300

    fin

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { App } from 'components/example-1/App';
-// import { App } from 'components/example-2/App';
-import { App } from 'components/example-3/App';
+import { App } from 'components/App';
 import './index.css';
+import { ThemeProvider } from 'styled-components';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={{}}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
